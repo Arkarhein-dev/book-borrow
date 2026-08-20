fbb3d36a3d89035fbef61a8dd56cd1c634aecd14
Arkarhein-dev <hein90510@gmail.com>
2026-08-20 17:01:00 +0630

    start lms angular

---
+++ lms/lms-rest/frontend/src/app/layout/components/page-header/page-header.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-page-header',
+  styleUrl: './page-header.css',
+  templateUrl: './page-header.html',
+})
+export class PageHeader {
+  
+}
